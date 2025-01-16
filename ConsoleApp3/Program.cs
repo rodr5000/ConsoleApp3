af5646a6733f190c70332f7ff95dc415e3279426
rodr5000 <160001564+rodr5000@users.noreply.github.com>
2025-01-16 09:41:20 +0200

    Add files via upload

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,10 +13,30 @@ public class Program
         Queue<int> qCopy = SetQCopy(q);
         Console.WriteLine(q);
         Console.WriteLine(qCopy);
-        q.Insert(17);
+        q.Insert(7);
         qCopy.Insert(13);
+        q.Insert(14);
+        q.Insert(7);
+        q.Insert(20);
+
+        Queue<int> q2 = new Queue<int>();
+
+        q2.Insert(1);
+        q2.Insert(3);
+        q2.Insert(2);
+        q2.Insert(-2);
+
+        bool checkPerfect = CheckQPerfect(q2);
+        Console.WriteLine(checkPerfect);
+        Console.WriteLine(q2);
+
+        bool check = CheckAround(q, 5);
+
+        Console.WriteLine(check);
+
         Console.WriteLine(q);
         Console.WriteLine(qCopy);
+
     }
 
     public static Queue<int> SetQCopy(Queue<int> q)
@@ -37,6 +57,74 @@ public class Program
             q.Insert(qTemp.Remove());
         }
         return qCopy;
+    }
+
+    public static bool CheckAround(Queue<int> q, int place) 
+    {
+        
+        bool check = false;
+        int right = 0;
+        int sum = 0;
+        int current = 0;
+        Queue<int> qCopy = SetQCopy(q);
+        Queue<int> qCopy2 = SetQCopy(q);
+        int count = 0;
+
+        while (!qCopy2.IsEmpty()) 
+        {
+            count++;
+            qCopy2.Remove();
+        }
+
+        if (place < count && place > 1)
+        {
+            for (int i = 1; i < place; i++)
+            {
+                right = qCopy.Remove();
+            }
+
+
+            current = qCopy.Remove();
+            sum = qCopy.Remove() + right;
+
+            if (current == sum)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+
+    public static bool CheckQPerfect(Queue<int> q) 
+    {
+        Queue<int> qCopy = SetQCopy(q);
+        int length = 0;
+        int count = 0;
+
+        while (!qCopy.IsEmpty()) 
+        {
+            length++;
+            qCopy.Remove();
+        }
+
+        for (int place = 2; place < length; place++) 
+        {
+            if (CheckAround(q, place)) 
+            {
+                count++;
+            }
+        }
+        if (count == length - 2)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+
+
     }
 
 
