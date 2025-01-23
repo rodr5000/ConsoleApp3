749bc6387535bf863ea9a0c61b590dd82f2636fb
rodr5000 <160001564+rodr5000@users.noreply.github.com>
2025-01-23 09:37:24 +0200

    Add files via upload

---
+++ ConsoleApp3/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLab
+{
+    public class Student
+    {
+        private int grade;
+        private string name;
+
+        public Student(int grade, string name) 
+        {
+            this.grade = grade;
+            this.name = name;
+        }
+
+        public int GetGrade() => grade;
+
+        public string GetName() => name;
+
+        public override string ToString() => $"name: {name}, grade: {grade}";
+    }
+}
